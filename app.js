8bfcb470dcb41b60fc86e617ae198957b86ed21a
zerobug110 <mahabojang110@gmail.com>
2020-09-25 00:05:28 +0000

    added date generator to the footer

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Variables
 const navLinks = document.querySelector('.nav__item');
-const hamburger = document.querySelector('.lines')
-const textBox = document.querySelector ('.text-box')
+const hamburger = document.querySelector('.lines');
+const textBox = document.querySelector ('.text-box');
+const date = document.getElementById('date');
 
 //eventListeners 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('nav__open');
-
 });
+
+date.innerHTML = new Date().getFullYear();
